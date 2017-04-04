36a7c74f8b833e0e4a453248443f1d01daf25afe
tzyirvo <gabo.menacho@gmail.com>
2017-04-04 18:07:51 -0400

    Adding firebase to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFire } from 'angularfire2';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +10,30 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app works!';
 }
+//https://github.com/angular/angularfire2
+//export class AppComponent {
+//  private limit: Subject<number> = new Subject<number>();
+//
+//  constructor(af: AngularFire) {
+//    af.database.list(`test`, {
+//      query: {
+//        limitToFirst: this.limit
+//      }
+//    }).subscribe((val) => console.log(val));
+//
+//    this.limit.next(1);
+//
+//    setTimeout(() => {
+//      this.limit.next(2);
+//    }, 2000);
+//
+//    setTimeout(() => {
+//      this.limit.next(3);
+//    }, 4000);
+//
+//    let random = Math.floor(Math.random() * 10);
+//    setTimeout(() => {
+//      af.database.object(`test/i0`).set(random);
+//    }, 6000);
+//  }
+//}

4205a48725b69fadd4701cf6768ab12d101719bb
tzyirvo <gabo.menacho@gmail.com>
2017-04-24 12:44:26 -0400

    next bus - implementation finished and routing to closer stop view

--- src/app/body/components/proximo-bus/proximo-bus.component.ts
+++ src/app/body/components/proximo-bus/proximo-bus.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
+import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-proximo-bus',
@@ -6,7 +8,11 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 })
 export class ProximoBusComponent implements OnInit {
 
-  constructor(private elRef:ElementRef) { }
+  stops$: FirebaseListObservable<any[]>;
+
+  constructor(public af: AngularFire, private router:Router, private elRef:ElementRef) {
+    this.stops$ = af.database.list(`paradas`)
+  }
 
   ngOnInit() {
   }
@@ -15,6 +21,10 @@ export class ProximoBusComponent implements OnInit {
     let classList = this.elRef.nativeElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.classList
     classList.remove('inicio-tab')
     classList.add('other-tab')
+  }
+
+  selectStop(stop: string) {
+    this.router.navigate(['parada-mas-cercana'], { queryParams: { stop: stop } });
   }
 
 }

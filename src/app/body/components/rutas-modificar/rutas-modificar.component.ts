9457642283b9e90bb8412f0f206fe7439945eec9
tzyirvo <gabo.menacho@gmail.com>
2017-04-23 22:29:10 -0400

    modify route - stops toggle on change

--- src/app/body/components/rutas-modificar/rutas-modificar.component.ts
+++ src/app/body/components/rutas-modificar/rutas-modificar.component.ts
@@ -15,6 +15,7 @@ export class RutasModificarComponent implements OnInit {
   public icon: string = "assets/images/marker.png"
   public center: any = "-16.500393,-68.123077"
   public zoom: any = 14
+  public routeSelected: boolean = false
 
   constructor(private elRef:ElementRef, public afService:AF, public af: AngularFire) {
     this.routes$ = af.database.list(`rutas`)
@@ -65,7 +66,48 @@ export class RutasModificarComponent implements OnInit {
   }
 
   toggleStop(event, stopKey) {
-    //@TODO
+    event.preventDefault()
+    let options = event.target.options
+    let length = options.length
+    let classList
+    let i
+    let selectedCls = 'selected'
+    let isSelected
+    if (!this.routeSelected) {
+      alert('Seleccione una ruta antes de modificar las paradas')
+      return
+    }
+    for (i = 0; i < length; i += 1) {
+      if (options[i].value === stopKey) {
+        classList = options[i].classList
+        isSelected = (options[i].className.indexOf(selectedCls) > -1)
+        classList[isSelected ? 'remove': 'add'](selectedCls)
+        options[i].selected = !isSelected
+        this.toggleStopSelection(stopKey, !isSelected)
+      }
+    }
+    event.target.value = undefined
+  }
+
+  toggleStopSelection(stopKey, addStop) {
+    let i
+    let stop
+    for (i = 0; i < this.positions.length; i += 1) {
+      if (this.positions[i].$key === stopKey) {
+        stop = this.positions[i]
+        break
+      }
+    }
+    if (addStop) {
+      this.selectedPositions.push(stop)
+      return
+    }
+    for (i = 0; i < this.selectedPositions.length; i += 1) {
+      if (this.selectedPositions[i].$key === stopKey) {
+        this.selectedPositions.splice(i, 1)
+        return
+      }
+    }
   }
 
   modifyRoute(event) {
@@ -82,13 +124,14 @@ export class RutasModificarComponent implements OnInit {
     for(i = 0; i < options.length; i += 1) {
       options[i].selected = (this.selectedPositions.indexOf(options[i].value) > -1)
     }
+    this.routeSelected = true
   }
 
   isStopinRoute(stopKey, paradas) {
     let i
     for(i = 0; i < paradas.length; i += 1) {
       if (stopKey === paradas[i].$value) {
-        return true;
+        return true
       }
     }
     return false
